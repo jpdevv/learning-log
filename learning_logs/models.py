cff4f7e9ab939a41a973f2295fd8b3924033a01d
jpdevv <joaopaulo.ctto1@gmail.com>
2024-06-26 09:42:14 -0300

    v0.0.1

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -4,3 +4,7 @@ class Topic(models.Model):
     """Um assunto sobre o qual o usuário está aprendendo"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        """Devolve uma repesentação em string do modelo"""
+        return self.text
